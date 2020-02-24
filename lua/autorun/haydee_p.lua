0d219b65d19b780dd3f270471224162d12e2cd35
Artem Ryazanov <artryazanov@gmail.com>
2020-02-24 17:20:40 +0300

    Initial Commit

---
+++ lua/autorun/haydee_p.lua
@@ -0,0 +1,36 @@
+/*///////////////////////////////////////////////////////////////////////////////
+//
+//Thank you for downloading this addon
+//Спасибо за загрузку этого аддона
+//
+//Эта модель была взята из проекта в GreenLight, за который вы можете проголосовать тут - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//This model was derived from the project the GreenLight, for which you can vote here - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//
+//Website - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//Вебсайт - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//
+// ███████████████████████████████
+// █─██─█────█──█──█────██───█───█
+// █─██─█─██─██───██─██──█─███─███
+// █────█────███─███─██──█───█───█
+// █─██─█─██─███─███─██──█─███─███
+// █─██─█─██─███─███────██───█───█
+// ███████████████████████████████
+//
+Авторы модели - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+Author model - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+
+███─█───█─████─████─████─███─███─████
+─█──██─██─█──█─█──█─█──█──█──█───█──██
+─█──█─█─█─████─█──█─████──█──███─█──██ - http://steamcommunity.com/id/sharkvil1337/
+─█──█───█─█────█──█─█─█───█──█───█──██
+███─█───█─█────████─█─█───█──███─████
+////////////////////////////////////////////////////////////////////////////////*/
+player_manager.AddValidModel( "Haydee[P.M]", "models/models/haydee/steam/player/haydee_player_model.mdl" )
+list.Set("PlayerOptionsModel", "Haydee[P.M]", "models/models/haydee/steam/player/haydee_player_model.mdl" )
+player_manager.AddValidHands( "Haydee[P.M]", "models/models/haydee/steam/weapon/arms/arms.mdl", 0, "00000000" )
+/*
+if SERVER then
+	resource.AddWorkshop("000000000")
+end
+*/
---
+++ lua/autorun/readme.lua
@@ -0,0 +1,33 @@
+/*///////////////////////////////////////////////////////////////////////////////
+//
+//Thank you for downloading this addon
+//Спасибо за загрузку этого аддона
+//
+//Эта модель была взята из проекта в GreenLight, за который вы можете проголосовать тут - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//This model was derived from the project the green light, for which you can vote here - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//
+//Website - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//Вебсайт - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+//
+// ███████████████████████████████
+// █─██─█────█──█──█────██───█───█
+// █─██─█─██─██───██─██──█─███─███
+// █────█────███─███─██──█───█───█
+// █─██─█─██─███─███─██──█─███─███
+// █─██─█─██─███─███────██───█───█
+// ███████████████████████████████
+//
+Авторы модели - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+Author model - http://steamcommunity.com/sharedfiles/filedetails/?id=735346310
+
+███─█───█─████─████─████─███─███─████
+─█──██─██─█──█─█──█─█──█──█──█───█──██
+─█──█─█─█─████─█──█─████──█──███─█──██ - http://steamcommunity.com/id/sharkvil1337/
+─█──█───█─█────█──█─█─█───█──█───█──██
+███─█───█─█────████─█─█───█──███─████
+////////////////////////////////////////////////////////////////////////////////*/
+
+
+/*
+Community Game Servers - vk.com/flaminggaming / flaminggaming.ru
+*/
